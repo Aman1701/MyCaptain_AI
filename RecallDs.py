95c9e9539253fa6dcdec49096d41e4cf82e1eec6
Aman kumar <53261649+Aman1701@users.noreply.github.com>
2020-06-23 21:29:27 +0530

    Create RecallDs.py
    Recalling Python Data Structures

---
+++ RecallDs.py
@@ -0,0 +1,24 @@
+# Assigning elements to different lists
+list1 = []
+list2 = []
+list1.append("Aman")
+list1.append("Cap_Lakshya")
+list2.append("MyCaptain")
+print(list1)
+print(list2)
+
+print('')
+
+# Accessing elements from a tuple
+tup = (17, 'Hello', 'Awesome')
+print("Element accessed is : ")
+print(tup[0])
+
+print('')
+
+# Deleting different dictionary elements
+myDict = {'Name' : 'Aman', 'Passion' : 'Learn New tech', 'Habbit' : 'Defense articles'}
+print(myDict)
+del myDict['Habbit']
+del myDict['Passion']
+print(myDict)
